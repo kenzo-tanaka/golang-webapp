d581905fd6996432c6fcf51fa22572e85f87c494
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-26 18:08:15 +0900

    fix: main関数を修正

--- chap1/chat/main.go
+++ chap1/chat/main.go
@@ -24,7 +24,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chats.html"})
+	http.Handle("/room", r)
+
+	go r.run()
+
+	// Webサーバーを起動する
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", nil)
 	}
