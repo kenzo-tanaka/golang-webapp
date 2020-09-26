41c38d4db200aa7d6af282a1d8481b83bbbf1c1a
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-26 17:51:12 +0900

    chore: コメント記載

--- chap1/chat/room.go
+++ chap1/chat/room.go
@@ -5,7 +5,7 @@ type room struct {
 	// forwardは他のclientに送信するメッセージを保持するためのチャネル
 	forward chan []byte
 
-	join    chan *client
-	leave   chan *client
-	clients map[*client]bool
+	join    chan *client     // チャットに参加しようとしているclientのためのチャネル
+	leave   chan *client     // チャットから退出しようとしているclientのためのチャネル
+	clients map[*client]bool // 在籍しているclientを保持
 }
