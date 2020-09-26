d3707e985a58a78abcd640426c79a694104be3da
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-26 17:55:46 +0900

    feat: 参加・退出・メッセージ送信などを管理するメソッドを追加

--- chap1/chat/room.go
+++ chap1/chat/room.go
@@ -9,3 +9,21 @@ type room struct {
 	leave   chan *client     // チャットから退出しようとしているclientのためのチャネル
 	clients map[*client]bool // 在籍しているclientを保持
 }
+
+func (r *room) run() {
+	for {
+		select {
+		// 参加
+		case client := <-r.join:
+			r.clients[client] = true
+		// 退出
+		case client := <-r.leave:
+			delete(r.clients, client)
+		// メッセージを送信
+		case msg := <-r.forward:
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
+}
