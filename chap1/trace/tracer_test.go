318e84057c6aabef69bde394d0d9f441354889f9
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-27 16:37:07 +0900

    test: tracer_test.go が全てパスするよう実装を修正

--- chap1/trace/tracer_test.go
+++ chap1/trace/tracer_test.go
@@ -1,7 +1,19 @@
 package trace
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestNew(t *testing.T) {
-	t.Error("まだテストを書いていません")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("Newからの戻り値がnil")
+	} else {
+		tracer.Trace("こんにちは")
+		if buf.String() != "こんにちは\n" {
+			t.Errorf("%sという誤った文字列が返されました。", buf.String())
+		}
+	}
 }
