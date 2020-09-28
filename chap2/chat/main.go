d0dd0c98be8d00024f0135f81c555baba1dbebeb
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-28 09:36:52 +0900

    fix: main.goの中でログイン中のユーザー情報を保存

--- chap2/chat/main.go
+++ chap2/chat/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/stretchr/gomniauth"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 // templ は1つのtemplateを表す
@@ -26,6 +27,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
 	t.templ.Execute(w, r)
 }
 
