bd1c3b38793ea8280b65fff5d102b56beb71db74
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-30 20:53:57 +0900

    feat: ログアウト機能の追加

--- chap3/chat/main.go
+++ chap3/chat/main.go
@@ -62,6 +62,16 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r) // /room/ ref: https://github.com/matryer/goblueprints/issues/72
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 
 	// get the room going
 	go r.run()
